3ea84413001624ad723deaba11db96adf8e72c04
omitsuhashi <osamu.mitsuhashi224@gmail.com>
2023-04-06 23:37:42 +0900

    feat item design

--- src/app/stores/[storeId]/_item.tsx
+++ src/app/stores/[storeId]/_item.tsx
@@ -1,14 +1,23 @@
 import { FetchItemDaoModel } from '@/dao/order';
+import { useState } from 'react';
 
 type Props = {
   item: FetchItemDaoModel;
 };
 
 export default function ItemComponent({ item }: Props) {
+  const [quantity, setQuantity] = useState(1);
   return (
     <>
-      <div className={'box'} data-testid={'item'}>
-        {item.name} - {item.price}
+      <div className='media'>
+        <div className='media-left'>fig</div>
+        <div className='media-content'>
+          <p className='title is-4'>{item.name}</p>
+          <p className='subtitle is-6'>desc</p>
+        </div>
+        <div className='media-right'>
+          <input className={'input'} type='text' value={quantity} />
+        </div>
       </div>
     </>
   );
