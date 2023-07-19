dd3a3ea110a8f2fa69606fdc83d37f7be446f10e
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2023-07-19 21:32:49 +0900

    feat memorize menu component

--- src/components/organisms/order/menu.tsx
+++ src/components/organisms/order/menu.tsx
@@ -2,7 +2,7 @@ import useSWR from 'swr';
 import { MenuItemType } from '@/types/store/order';
 import { STORE_API } from '@/constants/api';
 import { ID } from '@/types';
-import { PropsWithoutRef } from 'react';
+import React, { PropsWithoutRef } from 'react';
 import styles from '@/styles/pages/store.module.scss';
 import MenuItem from '@/components/organisms/order/menu-item';
 import Loading from '@/components/atoms/loading';
@@ -12,7 +12,7 @@ type Props = {
   genreId?: ID;
 };
 
-export default function Menu({ storeId, genreId }: PropsWithoutRef<Props>) {
+function Menu({ storeId, genreId }: PropsWithoutRef<Props>) {
   const { data, error, isLoading } = useSWR<Array<MenuItemType>>(
     STORE_API.getMenu(storeId, genreId),
   );
@@ -32,3 +32,5 @@ export default function Menu({ storeId, genreId }: PropsWithoutRef<Props>) {
     );
   });
 }
+
+export default React.memo(Menu);
