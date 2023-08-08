a175fdd864143bea7334736e86893e9427221036
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2023-08-09 06:52:44 +0900

    upgrade packages

--- __tests__/jest.tsx
+++ __tests__/jest.tsx
@@ -1,13 +1,10 @@
 import { render } from '@testing-library/react';
 import { PropsWithChildren, ReactElement } from 'react';
 import { TestSwrConfig } from '@/libs/swr';
-import { RecoilRoot } from 'recoil';
 
 const testRender = (ui: ReactElement) => {
   const wrapper = (props: PropsWithChildren) => (
-    <RecoilRoot>
-      <TestSwrConfig>{props.children}</TestSwrConfig>
-    </RecoilRoot>
+    <TestSwrConfig>{props.children}</TestSwrConfig>
   );
   render(ui, { wrapper });
 };
